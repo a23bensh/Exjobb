53e9c00823ed3aa41482d2c84e50a2e1a3293144
Ben <a23bensh@student.his.se>
2026-03-15 13:59:39 +0100

    Skapat databas uppkoppling till index.js

--- Node.js/index.js
+++ Node.js/index.js
@@ -1,5 +1,25 @@
-const app = require('express')();
+const express = require('express');
+const mysql = require('mysql2');
+
+const app = express();
 const port = 3000;
+
+// Skapa en anslutning till MySQL-databasen
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'exjobbdb'
+});
+
+// Anslut till databasen
+connection.connect((err) => {
+  if (err) {
+    console.error('Fel vid anslutning till databasen:', err);
+    return;
+  } 
+  console.log('Ansluten till MySQL-databasen!');
+});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
