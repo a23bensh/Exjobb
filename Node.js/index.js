f8a48c35a67b3a51b293c7a9734189c1b47f7136
Ben <a23bensh@student.his.se>
2026-03-25 13:26:28 +0100

    Ändrat språk för att matcha dataset

--- Node.js/index.js
+++ Node.js/index.js
@@ -64,10 +64,10 @@ app.get('/books/:id', (req, res) => {
         <body>
           <h2>${book.title}</h2>
           <p>by ${book.authors}</p>
-          <p><strong>Kategori:</strong> ${book.categories}</p>
-          <p><strong>År:</strong> ${book.published_year}</p>
-          <p><strong>Sidor:</strong> ${book.num_pages}</p>
-          <p><strong>Beskrivning:</strong> ${book.description}</p>
+          <p><strong>Category:</strong> ${book.categories}</p>
+          <p><strong>Year:</strong> ${book.published_year}</p>
+          <p><strong>Pages:</strong> ${book.num_pages}</p>
+          <p><strong>Description:</strong> ${book.description}</p>
           <button onclick="location.href='/'">Tillbaka</button>
         </body>
       </html>
